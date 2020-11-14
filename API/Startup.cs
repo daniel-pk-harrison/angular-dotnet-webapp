02c7fe638b8d1d64e03551086863ca7ec3cb6de0
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-11-14 00:08:39 -0500

    configured https client

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,7 +53,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); //string is origin of frontend
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //string is origin of frontend
 
             app.UseAuthorization();
 
