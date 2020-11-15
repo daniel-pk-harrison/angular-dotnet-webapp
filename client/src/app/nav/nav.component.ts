61ae0950ac5634d03d42d1ed95b3e652e523ed3c
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-11-14 22:02:46 -0500

    added home component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/users';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,19 +10,16 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn!: boolean;
 
-  constructor(private accountService: AccountService) {
+  constructor(public accountService: AccountService) {
    }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
   login () {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     })
@@ -28,15 +27,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    })
   }
 
 }
