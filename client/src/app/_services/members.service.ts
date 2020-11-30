0b2bbdb4b539de38d8481cf3e094fb8ce83ffcd7
Daniel Harrison <daniel.harrison@ihsmarkit.com>
2020-11-30 15:48:16 -0700

    cached the parameters by moving them to the memberservice

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,11 +2,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { ReturnStatement } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
+import { User } from '../_models/users';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +17,28 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
+  }
+
+  getUserParams() {
+    return this.userParams;
+  }
+
+  setUserParams(params: UserParams) {
+    this.userParams = params;
+  }
+
+  resetUserParams() {
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+  }
 
   getMembers(userParams: UserParams) {
     //try and pull data from cache, userparams are the key
