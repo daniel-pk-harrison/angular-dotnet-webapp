2150d7b3ad081303c04fd9da8cd38f62cda1c14a
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-11-29 19:51:15 -0500

    created a pagedlist class that will be used for pagination

---
+++ API/Helpers/PagedList.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize); //num of pages = total count/num per page
+            PageSize = pageSize;
+            TotalCount = count;
+            AddRange(items);
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(); //take the number of records skipping the first N where N is the number of items based on the pagesize and pagenumber
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
