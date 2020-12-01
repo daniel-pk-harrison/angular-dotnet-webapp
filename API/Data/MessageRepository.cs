88616a2ba8b9bef78b33ce62b9fc46fe71498c96
Daniel Harrison <daniel.harrison@ihsmarkit.com>
2020-12-01 08:52:21 -0700

    Added GetMessage endpoint to MessageController Updated messagerepo with GetMessagesForUser Added a MessageParams class

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -1,38 +1,54 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class MessageRepository : IMessageRepository
     {
-        private readonly DataContext _dataContext;
-        public MessageRepository(DataContext dataContext)
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public MessageRepository(DataContext context, IMapper mapper)
         {
-            _dataContext = dataContext;
+            _mapper = mapper;
+            _context = context;
         }
 
         public void AddMessage(Message message)
         {
-            _dataContext.Messages.Add(message);
+            _context.Messages.Add(message);
         }
 
         public void DeleteMessage(Message message)
         {
-            _dataContext.Messages.Remove(message);
+            _context.Messages.Remove(message);
         }
 
         public async Task<Message> GetMessage(int id)
         {
-            return await _dataContext.Messages.FindAsync(id);
+            return await _context.Messages.FindAsync(id);
         }
 
-        public async Task<PagedList<MessageDto>> GetMessagesForUser()
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new System.NotImplementedException();
+            var query = _context.Messages.OrderByDescending(m => m.MessageSent).AsQueryable();
+
+            query = messageParams.Container switch
+            {
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username),
+                _ => query.Where(u => u.Recipient.UserName == messageParams.Username && u.DateRead == null) //underscore is default case in new switch
+            };
+
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public async Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
@@ -42,7 +58,7 @@ namespace API.Data
 
         public async Task<bool> SaveAllAsync()
         {
-            return await _dataContext.SaveChangesAsync() > 0;
+            return await _context.SaveChangesAsync() > 0;
         }
     }
 }
