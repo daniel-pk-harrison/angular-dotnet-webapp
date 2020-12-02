d9c2a804beacb8bcd3f39c8bcc3f7d158057838a
Daniel Harrison <daniel.harrison@ihsmarkit.com>
2020-12-01 17:29:14 -0700

    Updated MessageRepository only return message not marked for deletion Updated MessagesController with a delete endpoint only deleting from DB if both parties marked to delete

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -71,5 +71,25 @@ namespace API.Controllers
 
             return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var username = User.GetUsername();
+
+            var message = await _messageRepository.GetMessage(id);
+
+            if (message.Sender.UserName != username && message.Recipient.UserName != username) return Unauthorized(); //only delete message to and from current user
+
+            if (message.Sender.UserName == username) message.SenderDeleted = true;
+
+            if (message.Recipient.UserName == username) message.RecipientDeleted = true;
+
+            if (message.SenderDeleted && message.RecipientDeleted) _messageRepository.DeleteMessage(message); //only delete if both parties marked to delete
+
+            if (await _messageRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting the message");
+        }
     }
 }
